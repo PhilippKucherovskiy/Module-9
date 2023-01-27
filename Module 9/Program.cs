a8bb4f82d5419a4f2794bce6403c2fd388b7b6bd
Philipp Kucherovskiy <philipp.kucherovskiy@gmail.com>
2023-01-27 12:21:29 +0300

    Добавьте файлы проекта.

---
+++ Module 9/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace DelegatePractices
+{
+    class MyException : Exception
+        {
+            public MyException(string message) : base(message) { }
+        }
+
+        class Program
+        {
+            static void Main()
+        { Exception[] exceptions = {
+            new DivideByZeroException(),
+            new IndexOutOfRangeException(),
+            new InvalidOperationException(),
+            new MyException("My custom exception"),
+            new ArgumentException()};
+
+                try
+                {
+                    for (int i = 0; i < exceptions.Length; i++)
+                    {
+                        throw exceptions[i];
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Finally block executed.");
+                }
+            }
+        }
+
+    
+}
